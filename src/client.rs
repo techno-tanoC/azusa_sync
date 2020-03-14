99e54028e2828036f8461fec4156732bba8f17d4
techno-tanoC <develop.tanoc@gmail.com>
2020-03-14 23:54:24 +0900

    update client

--- src/client.rs
+++ src/client.rs
@@ -3,6 +3,7 @@ use std::io::{Read, Write, Seek};
 
 use super::progress::Progress;
 use super::table::Table;
+use super::lock_copy::LockCopy;
 use super::error::{Error, Result};
 
 #[derive(Debug, Clone)]
@@ -17,22 +18,25 @@ impl Client {
         Client(client)
     }
 
-    pub fn start<W: Write + Read + Seek + Clone>(&self, table: &Table<String, Progress<W>>, url: &str, buf: W, name: impl ToString) -> Result<()> {
-        let mut pg = Progress::new(name, buf);
+    pub fn start<W>(&self, table: &Table<String, Progress<W>>, buf: W, url: &str, name: impl AsRef<str>, ext: impl AsRef<str>) -> Result<()>
+        where W: Read + Write + Seek + Clone
+    {
+        let mut pg = Progress::new(name.as_ref(), buf);
         let _add = table.add(pg.clone());
         let mut res = self.0.get(url).send()?;
-        Client::download(&mut pg, &mut res)?;
-        // copy
+        Client::download(&mut res, &mut pg)?;
+        // todo fix
+        LockCopy::new(&".").copy(&mut pg, name.as_ref(), ext.as_ref())?;
         Ok(())
     }
 
-    fn download<W: Write>(pg: &mut Progress<W>, res: &mut blocking::Response) -> Result<()> {
+    fn download<W: Write>(res: &mut blocking::Response, pg: &mut Progress<W>) -> Result<()> {
         if res.status().is_success() {
             if let Some(cl) = Self::content_length(res) {
                 pg.set_total(cl);
             }
 
-            // it can return canceled error.
+            // todo it can return canceled error.
             std::io::copy(res, pg)?;
             Ok(())
         } else {
@@ -44,5 +48,15 @@ impl Client {
         res.headers()
             .get(header::CONTENT_LENGTH)?
             .to_str().ok()?.parse().ok()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // todo
+    #[test]
+    fn content_length_test() {
     }
 }
